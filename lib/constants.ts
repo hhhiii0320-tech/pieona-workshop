c2cec155f83325d36c0d2c722071194c8ff11f2c
hhhiii0320-tech <hhhiii0320@gmail.com>
2026-07-29 14:09:05 +0900

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -475,8 +475,8 @@ export const participantsSection = {
 export const workshopStatus = {
   capacity: 20,
   minimumOpen: 8,
-  currentApplicants: 3,
-  packageApplicants: 3,
+  currentApplicants: 5,
+  packageApplicants: 5,
   // 확정 후 "2026-08-15" 형태로 입력
   earlyBirdDeadline: "2026-08-02",
 };
